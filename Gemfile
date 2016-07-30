78d8231a659d86feb3ea2ee4bd080c13684c08d7
Nick Harrison <nick@ozguild.com.au>
2016-07-30 12:48:32 +1000

    pulling data from FTP server and parsing it into tasty ruby

--- Gemfile
+++ Gemfile
@@ -30,9 +30,6 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-gem 'nokogiri'
-gem 'colorize'
-gem 'httparty'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- csiro_scrape.rb
+++ csiro_scrape.rb
@@ -1,67 +1,50 @@
 #!/usr/bin/env ruby
 
 require 'open-uri'
-require 'nokogiri'
 require 'csv'
-require 'net/ftp'
-require 'colorize'
-require 'httparty'
 
 BASE_URL = 'ftp://ftp.ga.gov.au/outgoing-emergency-imagery/sentinel/'
 
-def scrape
-
+def getFilePaths
   uri = URI.parse(BASE_URL)
-  out_dir = './temp'
-  Dir.mkdir(out_dir) unless Dir.exists?(out_dir)
   dirs = ['AVHRR', 'hotspots', 'MODIS', 'VIIRS']
+  path_list = []
 
   dirs.each do |d|
-    file_list = []
     begin
       Net::FTP.open(uri.host) do |ftp|
         ftp.login
         ftp.chdir(uri.path + d)
-        file_list << ftp.list('*hotspots.txt')
+        lines = ftp.list('*hotspots.txt')
+
+        lines.each do |l|
+          name = l.split(" ").delete_if { |w| !w.include?(".txt")}
+          path = BASE_URL + d + '/' + name[0]
+          p path
+          path_list << path
+        end
       end
     rescue Exception => e
       puts "Exception: '#{e}'. FTP Failed."
-      return
     end
-
-    p file_list
-
-    path = "#{uri}#{d}"
-    open(path) { |f|
-      if (f.basename.includes?('hotpots.txt'))
-        File.open("#{out_dir}/#{f.basename}.csv", 'w') do |o|
-            o.write(f)
-        end
-      end
-    }
   end
+
+  path_list
 end
 
-def processFile(file)
-end
-
-def getData
-  source_dir = './datasets'
-  out_dir = './temp'
-  Dir.mkdir(out_dir) unless Dir.exists?(out_dir)
-  dirs = ['AVHRR', 'hotspots', 'MODIS', 'VIIRS']
-
+def getData(paths)
   CSV::Converters[:blank_to_nil] = lambda do |field|
     field && field.empty? ? nil : field
   end
 
-  dirs.each do |d|
-    Dir.glob(File.join("#{source_dir}/#{d}/*hotspots.txt")).each do |f|
-      csv = CSV.new(File.open(f), :headers => true, :header_converters => :symbol, :converters => [:all, :blank_to_nil])
+  paths.each do |path|
+    open(path) { |f|
+      csv = CSV.new(f, :headers => true, :header_converters => :symbol, :converters => [:all, :blank_to_nil])
       p csv.to_a.map {|row| row.to_hash }
-    end
+    }
   end
 
 end
 
-getData
+paths = getFilePaths
+getData(paths)
