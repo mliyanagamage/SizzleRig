12e3fad82fd6610864c877ccc292349702c4f469
Migara Liyanagamage <mliyanagamage@gmail.com>
2016-07-31 08:33:59 +1000

    add routes

--- app/controllers/heatmap_controller.rb
+++ app/controllers/heatmap_controller.rb
@@ -4,8 +4,10 @@ class HeatmapController < ApplicationController
   end
 
   def data
-    start_time = Date.parse(params[:date]).beginning_of_week
+    start_time = Date.parse(params[:date]).beginning_of_week.to_time
     end_date = start_time.end_of_week
+
+    
 
   end
 end
