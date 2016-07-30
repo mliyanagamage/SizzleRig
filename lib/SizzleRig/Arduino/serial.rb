522eaade7c5332ba50b74f8d9de1e120784d8d9c
Migara Liyanagamage <mliyanagamage@gmail.com>
2016-07-30 14:57:01 +1000

    add ruby interface to serial port.. THIS REQUIRES MINICOM BE INSTALLED

---
+++ lib/SizzleRig/Arduino/serial.rb
@@ -0,0 +1,16 @@
+module SizzleRig::Arduino
+  class Serial
+    include Singleton
+    
+    def initialize
+      @device = ENV["DEVICE_PATH"] || "/dev/tty.usbmodem1411"
+      @io = ::IO.popen("minicom -b 9600 -D #{@device}", 'w+')
+    end
+    
+    def rotate(percentage)
+      raise ArgumentError, "Percentage must be between 0.0 and 1.0" unless percentage > 0.0 && percentage <= 1.0
+      
+      io.puts percentage.to_s
+    end
+  end
+end
