c60b21050f6d270114813c09780dcad2a9014939
Migara Liyanagamage <mliyanagamage@gmail.com>
2016-07-31 08:42:52 +1000

    add heatmap controller code

--- app/controllers/heatmap_controller.rb
+++ app/controllers/heatmap_controller.rb
@@ -4,10 +4,15 @@ class HeatmapController < ApplicationController
   end
 
   def data
-    start_time = Date.parse(params[:date]).beginning_of_week.to_time
-    end_date = start_time.end_of_week
+    if params[:date].present?
+      start_time = Date.parse(params[:date]).beginning_of_week.to_time
+    else
+      start_time = Time.now.beginning_of_week
+    end
+    end_time = start_time.end_of_week
 
-    
+    @data = SentinelDatum.where(datetime: start_time..end_time).select(:latitude, :longitude, :power, :id)
 
+    render json: { data: @data }, status: 200
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'home#index'
   get '/heatmap' => 'heatmap#index'
+  get '/heatmap/data' => 'heatmap#data'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
