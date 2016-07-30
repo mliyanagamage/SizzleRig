22cc46891b8c2256810db128ab0fcb33c6716288
Migara Liyanagamage <mliyanagamage@gmail.com>
2016-07-31 08:57:58 +1000

    add flame route for changing flame level

---
+++ app/controllers/flame_controller.rb
@@ -0,0 +1,12 @@
+class FlameController < ApplicationController
+
+  def set_level
+    if params[:level].present?
+      fire_level = params[:level].to_f
+    else
+      fire_level = 0.01
+    end
+
+    SizzleRig::Arduino::Serial.rotate(fire_level)
+  end
+end
---
+++ test/controllers/flame_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FlameControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
