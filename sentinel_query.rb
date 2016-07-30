78962110fbe05b3200660a6e080d2984e64b4976
Nick Harrison <nick@ozguild.com.au>
2016-07-30 18:34:34 +1000

    printing out properties

--- sentinel_query.rb
+++ sentinel_query.rb
@@ -9,15 +9,23 @@ END_TIME_QUERY_PREFIX = '%20AND%20'
 def getData
   years = Array(2002..2016)
   months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+  total = 0
 
   years.each do |y|
     months.each do |m|
       url = "#{BASE_URL}australian_state#{START_TIME_QUERY_PREFIX}#{y}-#{m}-01T00%3A00%3A00#{END_TIME_QUERY_PREFIX}#{y}-#{m}-31T23%3A59%3A59"
       resp = HTTParty.get(url)
       json = JSON.parse(resp.body)
-      p json['features'][0]
+      
+      features = json['features']
+      features.each do |f|
+        p f['properties']
+      end
+
     end
   end
+
+  p "Grand Total: #{total}"
 end
 
 getData
