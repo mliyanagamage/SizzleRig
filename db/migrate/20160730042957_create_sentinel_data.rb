5806a0c935091e19d922b3bb2aa9da83d6520295
Nick Harrison <nick@ozguild.com.au>
2016-07-30 15:07:40 +1000

    added sentinel data fields

--- db/migrate/20160730042957_create_sentinel_data.rb
+++ db/migrate/20160730042957_create_sentinel_data.rb
@@ -1,7 +1,14 @@
 class CreateSentinelData < ActiveRecord::Migration[5.0]
   def change
     create_table :sentinel_data do |t|
-
+      t.decimal :latitude, :precision => 10, :scale => 7
+      t.decimal :longitude, :precision => 10, :scale => 7
+      t.decimal :temp_k, :precision => 4, :scale => 1
+      t.decimal :sh, :precision => 4, :scale => 3
+      t.decimal :sl, :precision => 4, :scale => 3
+      t.integer :confidence
+      t.datetime :datetime
+      t.string :offset
       t.timestamps
     end
   end
