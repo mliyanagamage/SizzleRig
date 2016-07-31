703ce4085c84cb47634c6a8fecb7d27133fb5b39
Migara Liyanagamage <mliyanagamage@gmail.com>
2016-07-31 14:17:01 +1000

    intergrate second dataset... yay!!

---
+++ app/helpers/act_fire_status_helper.rb
@@ -0,0 +1,2 @@
+module ActFireStatusHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get '/heatmap/data' => 'heatmap#data'
   post '/fire' => 'fire#set_level'
 
+  get '/act' => 'act_fire_status#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
