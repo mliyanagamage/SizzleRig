f8413cf71a5f15f0450245b32f6685a28b89f0d3
Migara Liyanagamage <mliyanagamage@gmail.com>
2016-07-31 14:47:43 +1000

    style datepicker more

--- app/assets/javascripts/heatmap.js
+++ app/assets/javascripts/heatmap.js
@@ -1,6 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+$(document).ready(function () {
+  $('#date').datepicker({
+    format: "yyyy-mm-dd"
+  });
+}); 
+
 var map;
 var styles = [
   {
