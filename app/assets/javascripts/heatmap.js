86bafa2e196c84d8ae4956bc585931e352b4b626
Migara Liyanagamage <mliyanagamage@gmail.com>
2016-07-31 14:51:09 +1000

    set default date, style submit

--- app/assets/javascripts/heatmap.js
+++ app/assets/javascripts/heatmap.js
@@ -3,8 +3,21 @@
 
 $(document).ready(function () {
   $('#date').datepicker({
-    format: "yyyy-mm-dd"
+    format: "yyyy-mm-dd",
+    autoclose: true
   });
+
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+
+  today = yyyy + '-' + mm + '-' + dd; // USE TODAY!!
+
+  $('#date').datepicker('update', today)
 }); 
 
 var map;
